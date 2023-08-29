36ada2771d7e401e6dbac2b700422bd03036a175
RodiaGiordano <rodiagiordano93@gmail.com>
2023-08-29 15:41:23 +0200

    add: milestone 0

--- script.js
+++ script.js
@@ -2,3 +2,17 @@
 // Stampare a schermo un messaggio all'interno di un h1, utilizzando i data.
 // Bonus:
 // Aggiungere alla pagina un'immagine, presa anch'essa da un data.
+
+
+const { createApp } = Vue
+
+createApp({
+  data() {
+    return {
+      message: 'hello world'
+    }
+
+    
+  }
+}).mount('#app')
+ 
